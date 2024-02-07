e906595edd3174c819cf42f3a5bf378aa0ae2d48
Francis Chung <francis.c.chung@gmail.com>
2024-02-07 16:09:12 +1100

    New RunningProcess project to simulate a normal process

---
+++ RunningProcess/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using RunningProcess;
+
+Console.WriteLine("Hello, World!");
+
+var runTask = new RunTask();
+runTask.StartCountJob();
+
+Console.ReadLine();
---
+++ RunningProcess/RunTask.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RunningProcess
+{
+    internal class RunTask
+    {
+        public void StartCountJob()
+        {
+            while (true)
+            {
+                Console.WriteLine("Running a long running Task counting til 1000000000 ");
+                // Introduce some computation to keep the loop busy
+                for (int i = 0; i < 1000000000; i++)
+                {
+                    double result = Math.Sqrt(i);
+                    Console.WriteLine($" Output: {result}");
+                }
+            }
+        }
+    }
+}
